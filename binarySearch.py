d736edf72c54acd19bf87e37e5500c3a8d65b58e
mitem9106 <mitem9106@gmail.com>
2020-06-08 13:23:18 -0400

    Recursive Binary Search algorithm in Python3.7.3

--- binarySearch.py
+++ binarySearch.py
@@ -1,13 +1,37 @@
-def binarySearch(array, x, r, n):
-    interval = int(n/2)
-    if array[interval] == x:
-        return interval
-    if array[interval] > x
+#returns the index of x in array if present, else -1
+def binarySearch(array, x, low, high):
+
+    #check base case
+    if high >= low:
+        mid = low + (high-low) // 2
+
+        #if element is present at the middle(interval) index
+        if array[mid] == x:
+            return mid
+
+        #if element is smaller than mid, then it must be
+        #present in the left subarray
+        elif array[mid] > x:
+            return binarySearch(array, x, low, mid-1)
+
+        #Else the element can only be present in the
+        #right subarray
+        else:
+            return binarySearch(array, x, mid + 1, high)
+    #Element is not present in the array
+    else:
+        return -1
+    
 def main():
     array = [2, 5, 8, 12, 16, 23, 38, 56, 72]
 
     print("Search: ")
     x = int(input())
-    n = len(array)
+    n = len(array)-1
 
-    binarySearch(array, x, n)
+    result = binarySearch(array, x, 0, n)
+
+    if result != -1:
+        print("Element is present at index % d" % result)
+    else:
+        print("Element is not present in array")
